d22a82e4384fabed57156875e26e459041f309eb
ANASS EL HARAZI <anaswins35@gmail.com>
2025-04-22 04:32:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let text = document.querySelector(".text");
 let btn = document.querySelector("button");
 
 btn.addEventListener("click", () => {
-    if (text.style.display === "none") {
+    if (text.style.display === "none" || text.style.display == '') {
         text.style.display = "block";
         btn.innerHTML = "CLOSE";
     } else {
